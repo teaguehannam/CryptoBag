7ccf3f5aa085712003f62485388eadca1c35942c
Teague Hannam <teaguehannam@gmail.com>
2022-03-31 18:06:39 -0600

    Hide data policy on page init

--- src/pages/data/_components/DataInfo.svelte
+++ src/pages/data/_components/DataInfo.svelte
@@ -1,7 +1,7 @@
 <script>
 	const indexedDBLink = 'https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB';
 	const githubLink = 'https://github.com/teaguehannam/CryptoBag';
-	let modalState = true;
+	let modalState = false;
 
 	function hdlModal() {
 		modalState = !modalState;
